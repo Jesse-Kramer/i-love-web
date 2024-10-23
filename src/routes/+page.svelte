fb1700c17f21f1d2a17ff71e477c0f4c5b3d3a88
OvenMagnetron <jessekramer15@gmail.com>
2024-10-23 22:50:46 +0200

    JK - Scroll animations - In progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,7 +22,27 @@
 </Header>
 
 <main>
-
+    <section>
+        <p>
+            <span>I create</span>
+            <span>cool stuff</span>
+            <span>with new</span>
+            <span>Technique's</span>
+        </p>
+    </section>
+    <section>
+        <ul class="qoutes">
+            <li>
+                <span>CREATIVITY HAS </span><span>NO</span><span>‎ </span><span>LIMITS</span>
+            </li>
+            <li>
+                <span>BUILD </span><span>YOUR</span><span>‎ DREAMS</span>
+            </li>
+            <li>
+                <span>LET'S </span><span>WORK </span><span>TOGETHER</span>
+            </li>
+        </ul>
+    </section>
 </main>
 
 <style>
